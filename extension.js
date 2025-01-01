dbdb2390f2bdd34b92de0db969553ec5461fdba1
Anssi Varjonen <anssi.varjonen@sanoma.com>
2025-01-01 12:39:07 +0200

    Add donation messages support

--- extension.js
+++ extension.js
@@ -1,31 +1,67 @@
+const { read } = require('fs');
 const fetch = require('node-fetch');
 
 const speedcontrolBundle = "nodecg-speedcontrol";
 
 module.exports = async function (nodecg) {
-
   const donateUrlReplicant = nodecg.Replicant("donateUrl", speedcontrolBundle);
   const donateAmountReplicant = nodecg.Replicant("donateAmount", speedcontrolBundle);
 
+  const donateMessagesUrlReplicant = nodecg.Replicant("donateMessagesUrl", speedcontrolBundle);
+  const donateMessages = nodecg.Replicant("donateMessages", speedcontrolBundle);
+
   let donateUrl = donateUrlReplicant.value;
+  let donateMessagesUrl = donateMessagesUrlReplicant.value;
 
-  const interval = setInterval(async () => {
+  if (!donateMessages.value) {
+    donateMessages.value = [];
+  }
+
+  setInterval(async () => {
     if (!donateUrl) return;
-  
-    const headers = {
-      "Content-Type": "application/json",
-      "X-Locale": "fi",
-      "X-Site-Id": "Dp9tjHj3gsVn"
-    };
 
-    const res = await fetch(donateUrl, { headers });
-    const data = await res.json();
+    const data = await fetchJson(donateUrl);
     const total = data.data.total_amount;
     
     donateAmountReplicant.value = total;
   }, 10000);
 
+  setInterval(async () => {
+    if (!donateMessagesUrl) return;
+
+    const data = await fetchJson(donateMessagesUrl);
+
+    const newDonates = data.data.filter(donate => (
+      !donateMessages.value.find(d => d.id === donate.id)
+    ));
+
+    const formatedDonates = newDonates.map(donate => ({
+      id: donate.id,
+      name: donate.name,
+      amount: donate.amount,
+      message: donate.message,
+      read: false
+    }));
+
+    donateMessages.value.push(...formatedDonates);
+  }, 10000);
+
   donateUrlReplicant.on("change", (val) => {
     donateUrl = val;
   });
+
+  donateMessagesUrlReplicant.on("change", (val) => {
+    donateMessagesUrl = val;
+  });
 };
+
+const fetchJson = async (url) => {
+  const headers = {
+    "Content-Type": "application/json",
+    "X-Locale": "fi",
+    "X-Site-Id": "Dp9tjHj3gsVn"
+  };
+  const res = await fetch(url, { headers });
+  return res.json();
+}
+
